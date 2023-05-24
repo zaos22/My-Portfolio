5d300f5fe8d34d89ccfaa48f30195cc03c5441e9
ZaynOssama <oussamazazou@iesmontsia.org>
2023-05-24 04:10:47 +0200

    75% About Component

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,9 +2,230 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  template: `<section>
+    <div class="container">
+      <div class="row space3">
+        <div class="col-12 pt-5 text-center">
+          <h1>Education</h1>
+        </div>
+        <div class="col-sm-6 pt-5">
+          <div class="container">
+            <section>
+              <div class="container">
+                <div class="row justify-content-center">
+                  <div class="col-md-8">
+                    <div class="contact-box">
+                      <div class="row justify-content-between">
+                        <div class="col-6 img-space">
+                          <div class="img1"></div>
+                        </div>
+                        <div class="col-6">
+                          <div class="contact-box-header">
+                            <h2>High grade formative cycle</h2>
+                          </div>
+                          <div class="contact-box-body">
+                            <div class="contact-option">
+                              <i class="fa-solid fa-graduation-cap"></i>
+                              <span>Web Application Development</span>
+                            </div>
+                            <div class="contact-option">
+                              <i class="fa-solid fa-school"></i>
+                              <a href="https://agora.xtec.cat/insmontsia/"
+                                ><span>IES MONTSIÀ - AMPOSTA</span></a
+                              >
+                            </div>
+                            <div class="contact-option">
+                              <i class="fa-regular fa-calendar-days"></i>
+                              <span>
+                                October 2021 - May 2023 (1 year and 7
+                                months)</span
+                              >
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+        <div class="col-sm-6 pt-5">
+          <div class="container">
+            <section>
+              <div class="container">
+                <div class="row justify-content-center">
+                  <div class="col-md-8">
+                    <div class="contact-box">
+                      <div class="row justify-content-between">
+                        <div class="col-6 img-space">
+                          <div class="img2"></div>
+                        </div>
+                        <div class="col-6">
+                          <div class="contact-box-header">
+                            <h2>Medium Grade Training Cycle</h2>
+                          </div>
+                          <div class="contact-box-body">
+                            <div class="contact-option">
+                              <i class="fa-solid fa-graduation-cap"></i>
+                              <span>Microcomputer systems and networks</span>
+                            </div>
+                            <div class="contact-option">
+                              <i class="fa-solid fa-school"></i>
+                              <a
+                                href="https://portal.edu.gva.es/iesjoancoromines/"
+                                ><span>IES JOAN COROMINES - BENICARLÓ</span></a
+                              >
+                            </div>
+                            <div class="contact-option">
+                              <i class="fa-regular fa-calendar-days"></i>
+                              <span>
+                                September 2019 - May 2021 (1 year and 8
+                                months)</span
+                              >
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+        <div class="col-12 pt-5 text-center">
+          <h1>Skills & Languages</h1>
+        </div>
+        <div class="col-sm-6 pt-5">
+          <div class="container">
+            <section>
+              <div class="container">
+                <div class="row justify-content-center">
+                  <div class="col-md-8">
+                    <div class="contact-box">
+                      <div class="contact-box-header">
+                        <h2>Skills</h2>
+                      </div>
+                      <div class="contact-box-body">
+                        <div class="row">
+                          <div class="col-3">
+                            <div class="mini-box">
+                              <i class="fab fa-java"></i>
+                              <span>Java</span>
+                            </div>
+                          </div>
+                          <div class="col-3">
+                            <div class="mini-box">
+                              <i class="fab fa-python"></i>
+                              <span>Python</span>
+                            </div>
+                          </div>
+                          <div class="col-3">
+                            <div class="mini-box">
+                              <i class="fab fa-js"></i>
+                              <span>JavaScript</span>
+                            </div>
+                          </div>
+                          <div class="col-3">
+                            <div class="mini-box">
+                              <i class="fab fa-php"></i>
+                              <span>PHP</span>
+                            </div>
+                          </div>
+                          <div class="col-3">
+                            <div class="mini-box">
+                              <i class="fab fa-html5"></i>
+                              <span>HTML</span>
+                            </div>
+                          </div>
+                          <div class="col-3">
+                            <div class="mini-box">
+                              <i class="fab fa-css3-alt"></i>
+                              <span>CSS</span>
+                            </div>
+                          </div>
+                          <div class="col-3">
+                            <div class="mini-box">
+                              <i class="fas fa-database"></i>
+                              <span>SQL</span>
+                            </div>
+                          </div>
+                          <div class="col-3">
+                            <div class="mini-box">
+                              <i class="fab fa-laravel"></i>
+                              <span>Laravel</span>
+                            </div>
+                          </div>
+                          <div class="col-3">
+                            <div class="mini-box">
+                              <i class="fab fa-angular"></i>
+                              <span>Angular</span>
+                            </div>
+                          </div>
+                          <div class="col-3">
+                            <div class="mini-box">
+                              <i class="fab fa-vuejs"></i>
+                              <span>Vue</span>
+                            </div>
+                          </div>
+                          <div class="col-3">
+                            <div class="mini-box">
+                              <i class="fab fa-react"></i>
+                              <span>React</span>
+                            </div>
+                          </div>
+                          <div class="col-3 pt-2">
+                            <div class="mini-box">
+                              <a href="/reach" class="info-link">
+                                <i class="fas fa-plus"></i>
+                              </a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+        <div class="col-sm-6 pt-5">
+          <div class="container">
+            <section>
+              <div class="container">
+                <div class="row justify-content-center">
+                  <div class="col-md-8">
+                    <div class="contact-box">
+                      <div class="contact-box-header">
+                        <h2>Languages</h2>
+                      </div>
+                      <div class="contact-box-body">
+                        <div class="contact-option">
+                          <i class="fas fa-envelope"></i>
+                          <span>Email: ozazou2001@gmail.com</span>
+                        </div>
+                        <div class="contact-option">
+                          <i class="fas fa-phone"></i>
+                          <span>Phone: +34617177042</span>
+                        </div>
+                        <div class="contact-option">
+                          <i class="fas fa-map-marker-alt"></i>
+                          <span>Address: Spain, Benicarló</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>`,
+  styleUrls: ['./about.component.css'],
 })
-export class AboutComponent {
-
-}
+export class AboutComponent {}
