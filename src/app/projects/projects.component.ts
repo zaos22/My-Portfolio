33ecaa4f897ea231c35ddb027ad19b1c7ec2e241
ZaynOssama <oussamazazou@iesmontsia.org>
2023-05-25 02:09:56 +0200

    END

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -2,9 +2,231 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
-  template: '',
-  styleUrls: ['./projects.component.css']
+  template: `<section class="p-wrapper">
+    <div class="container">
+      <div class="row space3">
+        <div class="col-12 text-center">
+          <h1>Recently</h1>
+        </div>
+        <div class="col-sm-6 pt-5">
+          <div class="container">
+            <section>
+              <div class="container">
+                <div class="row justify-content-center">
+                  <div class="col-md-8">
+                    <div class="contact-box">
+                      <div class="row justify-content-between">
+                        <div class="col-6 img-space">
+                          <div class="img15"></div>
+                        </div>
+                        <div class="col-6 pt-5">
+                          <div class="contact-box-header">
+                            <h2>Portfolio</h2>
+                          </div>
+                          <div class="contact-box-body">
+                            <div class="contact-option">
+                              <a href="/home"
+                                ><i class="fa-solid fa-eye"></i
+                              ></a>
+                            </div>
+                            <div class="contact-option">
+                              <span> Developed with: Angular JS </span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+        <div class="col-sm-6 pt-5">
+          <div class="container">
+            <section>
+              <div class="container">
+                <div class="row justify-content-center">
+                  <div class="col-md-8">
+                    <div class="contact-box">
+                      <div class="row justify-content-between">
+                        <div class="col-6 img-space">
+                          <div class="img11"></div>
+                        </div>
+                        <div class="col-6 pt-5">
+                          <div class="contact-box-header">
+                            <h2>Virtualio</h2>
+                          </div>
+                          <div class="contact-box-body">
+                            <div class="contact-option">
+                              <a href="https://virtualiogs.iesmontsia.org/login"
+                                ><i class="fa-solid fa-eye"></i
+                              ></a>
+                            </div>
+                            <div class="contact-option">
+                              <span> Developed with: Laravel and Vue JS </span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+        <div class="col-12 pt-5 text-center">
+          <h1>All Projects</h1>
+        </div>
+        <div class="col-sm-6 pt-5">
+          <div class="container">
+            <section>
+              <div class="container">
+                <div class="row justify-content-center">
+                  <div class="col-md-8">
+                    <div class="contact-box">
+                      <div class="row justify-content-between">
+                        <div class="col-6 img-space">
+                          <div class="img12"></div>
+                        </div>
+                        <div class="col-6 pt-5">
+                          <div class="contact-box-header">
+                            <h2>Maesports</h2>
+                          </div>
+                          <div class="contact-box-body">
+                            <div class="contact-option">
+                              <a
+                                href="https://aunclic.shop/content/agencia-de-viajes-maesports"
+                                ><i class="fa-solid fa-eye"></i
+                              ></a>
+                            </div>
+                            <div class="contact-option">
+                              <span>
+                                Developed with: Wordpress and Prestashop
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+        <div class="col-sm-6 pt-5">
+          <div class="container">
+            <section>
+              <div class="container">
+                <div class="row justify-content-center">
+                  <div class="col-md-8">
+                    <div class="contact-box">
+                      <div class="row justify-content-between">
+                        <div class="col-6 img-space">
+                          <div class="img13"></div>
+                        </div>
+                        <div class="col-6 pt-5">
+                          <div class="contact-box-header">
+                            <h2>Laboratorios J.CID</h2>
+                          </div>
+                          <div class="contact-box-body">
+                            <div class="contact-option">
+                              <a
+                                href="https://aunclic.shop/content/laboratorio-jcid"
+                                ><i class="fa-solid fa-eye"></i
+                              ></a>
+                            </div>
+                            <div class="contact-option">
+                              <span>
+                                Developed with: Wordpress and Prestashop
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+        <div class="col-sm-6 pt-5">
+          <div class="container">
+            <section>
+              <div class="container">
+                <div class="row justify-content-center">
+                  <div class="col-md-8">
+                    <div class="contact-box">
+                      <div class="row justify-content-between">
+                        <div class="col-6 img-space">
+                          <div class="img14"></div>
+                        </div>
+                        <div class="col-6 pt-5">
+                          <div class="contact-box-header">
+                            <h2>Gestoría ibe</h2>
+                          </div>
+                          <div class="contact-box-body">
+                            <div class="contact-option">
+                              <a
+                                href="https://aunclic.shop/content/gestoria-ibe"
+                                ><i class="fa-solid fa-eye"></i
+                              ></a>
+                            </div>
+                            <div class="contact-option">
+                              <span>
+                                Developed with: Wordpress and Prestashop
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+        <div class="col-sm-6 pt-5">
+          <div class="container">
+            <section>
+              <div class="container">
+                <div class="row justify-content-center">
+                  <div class="col-md-8">
+                    <div class="contact-box">
+                      <div class="row justify-content-between">
+                        <div class="col-6 img-space">
+                          <div class="img11"></div>
+                        </div>
+                        <div class="col-6 pt-5">
+                          <div class="contact-box-header">
+                            <h2>Virtualio</h2>
+                          </div>
+                          <div class="contact-box-body">
+                            <div class="contact-option">
+                              <a href="https://virtualiogs.iesmontsia.org/login"
+                                ><i class="fa-solid fa-eye"></i
+                              ></a>
+                            </div>
+                            <div class="contact-option">
+                              <span> Developed with: Laravel and Vue JS </span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>`,
+  styleUrls: ['./projects.component.css'],
 })
-export class ProjectsComponent{
-
-}
+export class ProjectsComponent {}
