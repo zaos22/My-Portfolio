6ffa9dc51400c5df9d9099428c0e8a7362a9d4a9
Oussama Zazou Abid <94825975+ZaynOssama@users.noreply.github.com>
2023-05-25 03:20:52 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+// Deshabilitar mensajes de error en la consola del navegador
+if (window) {
+  window.console.error = () => {};
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
